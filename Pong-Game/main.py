11c9ea763e14e389495b2ec30a047b93e53c9310
Samarth-B-8 <samarthb816@gmail.com>
2025-09-22 21:00:08 +0530

    Updated the display and fixed the winning score

--- Pong-Game/main.py
+++ Pong-Game/main.py
@@ -12,6 +12,7 @@ screen.title("The Pong Game")
 screen.bgcolor("Black")
 screen.tracer(0)
 
+
 net = Turtle()
 net.color("White")
 net.penup()
@@ -33,37 +34,49 @@ ball = Ball()
 # Create a Scorecard
 scoreCard = Scoreboard()
 
+# The controlling condition of the game
+game_is_on = False
+is_match_on = True
+def start_game():
+    global game_is_on
+    game_is_on = True
+
 # Event listeners for the paddle movement
 screen.listen()
 screen.onkey(fun=right_pad.go_up, key="Up")
 screen.onkey(fun=right_pad.go_down, key="Down")
 screen.onkey(fun=left_pad.go_up, key='w')
 screen.onkey(fun=left_pad.go_down, key='s')
+screen.onkey(fun=start_game, key="space")
 
-
-game_is_on = True
-
-while game_is_on:
-    sleep(0.1)
+while is_match_on:
+    sleep(0.06)
     screen.update()
-    ball.movement()
 
-    # Detecting the walls and making the ball bounce
-    if ball.ycor() > 270 or ball.ycor() < -270:
-        ball.bounce_y()
-    
-    # Detecting the paddle and making the ball bounce
-    if (ball.xcor() > 320 and ball.distance(right_pad) < 50) or (ball.xcor() < -330 and ball.distance(left_pad) < 50):
-        ball.bounce_x()
+    if game_is_on:
+        ball.movement()
 
-    # Detection of the ball missing the paddle
-    if ball.xcor() > 400:
-        ball.reset_movement()
-        scoreCard.l_point()
+        # Detecting the walls and making the ball bounce
+        if ball.ycor() > 270 or ball.ycor() < -270:
+            ball.bounce_y()
+        
+        # Detecting the paddle and making the ball bounce
+        if (ball.xcor() > 320 and ball.distance(right_pad) < 50) or (ball.xcor() < -330 and ball.distance(left_pad) < 50):
+            ball.bounce_x()
 
-    if ball.xcor() < -400:
-        ball.reset_movement()
-        scoreCard.r_point()
+        # Detection of the ball missing the paddle
+        if ball.xcor() > 400:
+            ball.reset_movement()
+            scoreCard.l_point()
+            game_is_on = False
 
+        if ball.xcor() < -400:
+            ball.reset_movement()
+            scoreCard.r_point()
+            game_is_on = False
+
+        if scoreCard.l_score == 11 or scoreCard.r_score == 11:
+            is_match_on = False
+            scoreCard.winner()
 
 screen.exitonclick()
